d9884504e520998f226ba718ff783579c38c2a37
Alexandr <sasha-bragin@mail.ru>
2023-03-06 22:23:34 +0300

    выдаёт сумму чисел от 1 до А

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает 
+// на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+
+
+
+int Prompt()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int SumNums ( int number)
+{
+    int sum = 0;
+    for (int i =0; i <= number; i++)
+        sum+=i;
+    return sum;
+}
+
+int number = Prompt(); // int a = Prompt(); -- было так. что за a? что тогда такое number
+Console.WriteLine(SumNums(number)); // Console.WriteLine(SumNums(a));                                                                               
+
+
+54 минута
+
