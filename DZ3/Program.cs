d07fc0799921b169205e4707c4b28b86fc5cc75d
Alexandr <sasha-bragin@mail.ru>
2023-03-10 00:42:00 +0300

    выдаёт сумму чисел от 1 до А

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿// Задача 29: Напишите программу, которая 
+// задаёт массив из 8 элементов и выводит 
+// их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+int Prompt()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size = Prompt();
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0,211);
+    }
+    return array;
+}
+
+int[] newarray = CreateArray(size);
+
+Console.WriteLine($"[{String.Join(", ", newarray)}]");
