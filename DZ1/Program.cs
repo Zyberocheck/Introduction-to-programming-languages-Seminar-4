72e6e09583f8e96b8bdef6c76d95842ec1b0c50c
Alexandr <sasha-bragin@mail.ru>
2023-03-09 21:10:28 +0300

    Дз

---
+++ DZ1/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Задача 25: Напишите цикл, который 
+// принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+//  3, 5 -> 243 (3⁵)
+//  2, 4 -> 16
+
+Console.WriteLine("Введите два числа, ая возвиду первое в степень второго: ");
+double a = Convert.ToInt32(Console.ReadLine());
+double b = Convert.ToInt32(Console.ReadLine());
+
+// 2 решения
+
+// for( int i = 1; i<b; i++)
+// {
+    
+//     mult = mult * a;
+
+
+// }
+
+double mult;
+
+mult = Math.Pow(  a,  b);
+Console.WriteLine(mult);
+
---
+++ Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// // Задача 30: Напишите программу, которая 
+// выводит массив из 8 элементов, заполненный 
+// нулями и единицами в случайном порядке.
+// // [1,0,1,1,0,1,0,0]
+
+
+int Prompt()
+{
+    Console.WriteLine("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int size = Prompt();
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0,2);
+    }
+    return array;
+
+}
+
+
+int[] newarray = CreateArray(size);
+
+Console.WriteLine($"[{String.Join(", ", newarray)}]");
+
+
