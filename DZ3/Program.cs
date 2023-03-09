5cf6fda1fc7d8f6ca73107c814a62ca2c7e65a1d
Alexandr <sasha-bragin@mail.ru>
2023-03-10 01:17:04 +0300

    массив из 8 элементов и выводит их на экран.

--- DZ3/Program.cs
+++ DZ3/Program.cs
@@ -13,19 +13,42 @@ int Prompt()
 
 int size = Prompt();
 
+// int[] CreateArray(int size)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(0,211);
+//     }
+//     return array;
+// }
+
+// int[] newarray = CreateArray(size);
+
+// Console.WriteLine($"[{String.Join(", ", newarray)}]");
+
+// Печалька конечно что задания так пересеклись...
+
+// Поэтому попробую еще раз)
+
+
+
+
 int[] CreateArray(int size)
 {
     int[] array = new int[size];
-    for(int i = 0; i < size; i++)
+    int i= 0;
+    while(i < size)
     {
-        array[i] = new Random().Next(0,211);
+        array[i] = new Random().Next(-100,101);
+        i++;
+
     }
     return array;
+  
 }
 
-int[] newarray = CreateArray(size);
+int[] arraylast = CreateArray(size);
 
-Console.WriteLine($"[{String.Join(", ", newarray)}]");
-
-// Печалька конечно что задания так пересеклись.
+Console.WriteLine($"[{String.Join(", ", arraylast)}]");
 
